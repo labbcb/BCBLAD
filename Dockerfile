277d3843ea1644cd53a170b4e92c3943343d272a
Benilton Carvalho <beniltoncarvalho@gmail.com>
2017-02-06 22:07:30 -0200

    First commit: BCB LAb Docker - BCBLAD

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+## Use R/Official
+FROM rocker/tidyverse:3.3.2
+
+## Install tools for course
+RUN apt-get update \
+    && apt-get install -y \
+          bedtools \
+##          igv \
+          libxml2-dev \
+	  libxslt-dev \
+          python-dev \
+          python-numpy \
+          python-pip \
+          samtools \
+    && rm -rf /var/lib/apt/lists/*	  
+
+RUN pip install MACS2
+
+WORKDIR /home/rstudio
+
+RUN mkdir -p /igv && \
+    cd /igv && \
+    wget --quiet http://www.broadinstitute.org/igv/projects/downloads/IGV_2.3.42.zip && \
+    unzip IGV_2.3.42.zip && \
+    rm IGV_2.3.42.zip && \
+    cd IGV_2.3.42/ && \
+    sed -i 's/Xmx2000/Xmx8000/g' igv.sh && \
+    cd /usr/bin && \
+    ln -s /igv/IGV_2.3.42/igv.sh ./igv
+
+RUN apt-get update \
+    && apt-get install -y \
+    libgl1-mesa-dev \
+    libglu1-mesa-dev \
+    libpng12-dev \
+    libx11-dev \
+    libxt-dev \
+    r-cran-bbmisc \
+    r-cran-matrixstats \
+    r-cran-rgl \
+    x11proto-core-dev
+
+COPY biocpkgs.R /home/rstudio/v2/
+## COPY myrepo /home/rstudio/v2/myrepo
+WORKDIR /home/rstudio/v2
+RUN Rscript biocpkgs.R
+WORKDIR /home/rstudio
+RUN rm -fr v2
---
+++ biocpkgs.R
@@ -0,0 +1,33 @@
+.libPaths(setdiff(.libPaths(), "/usr/lib/R/library"))
+          
+## baseLocalRepo <- "myrepo"
+## localRrepo <- file.path(baseLocalRepo, "src/contrib")
+## fullPath <- file.path(getwd(), baseLocalRepo)
+
+## dir.create(localRrepo, recursive=TRUE)
+
+pkgs <- c('Biostrings', 'ShortRead', 'DESeq', 'edgeR','biomaRt',
+          'TxDb.Hsapiens.UCSC.hg19.knownGene', 'Rsamtools',
+          'BSgenome.Hsapiens.UCSC.hg19',
+          'BSgenome', 'GenomicAlignments',
+          'GenomicRanges',
+          'pasillaBamSubset', 'pasilla',
+          'rtracklayer', 'ggbio', 'vsn',
+          'gplots','RColorBrewer','chipseq', 'Rsubread',
+          'htSeqTools','limma','org.Hs.eg.db',
+          'Rcade', 'ChIPQC', 'ChIPpeakAnno',
+          'affy', 'affyPLM', 'limma', 'hthgu133pluspmcdf', 'hgu133plus2.db', 'genefilter', 'GSEABase', 'KEGG.db', 'Category', 'ReportingTools',
+          'crlmm', 'hapmapsnp6', 'genomewidesnp6Crlmm', 'illuminaHumanv1.db', 'GGdata', 'snpStats'
+          )
+
+library(BiocInstaller)
+## library(tools)
+## avail = available.packages(contrib.url(BiocInstaller::biocinstallRepos()))
+## deps = tools::package_dependencies(pkgs, avail, recursive=TRUE,)
+## download.packages(unique(c(pkgs, unlist(unname(deps)))), localRrepo,
+##     repos=biocinstallRepos())
+## tools::write_PACKAGES(localRrepo)
+
+## install.packages(pkgs, repo=paste0('file://', fullPath), type='source')
+
+biocLite(pkgs, ask=FALSE)
